92da4577c1e564d926f6c08c4f19655e9a09363a
akshatj125 <akshatj@argusoft.com>
2023-07-13 12:52:27 +0530

    signupCreated

--- Writers-Corner/src/app/app-routing.module.ts
+++ Writers-Corner/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
 
   {path:"", redirectTo:"/home",pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"login", component:LoginComponent},
+  {path:"signup", component:SignupComponent}
 
-const routes: Routes = [];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -16,6 +20,6 @@ const routes: Routes = [];
 export class AppRoutingModule { }
 export const routingComponents=[
   HomeComponent,
-  LoginComponent
-
+  LoginComponent,
+  SignupComponent
 ]
