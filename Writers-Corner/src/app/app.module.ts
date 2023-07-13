7465da448259f02cddc11a6fbaf93548a8fcc476
Sufyan-007 <sgazdhar@argusoft.com>
2023-07-13 11:03:10 +0530

    Added blogs and header

--- Writers-Corner/src/app/app.module.ts
+++ Writers-Corner/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { HeaderComponent }  from './header/header.component'
+import { BlogsComponent } from './blogs/blogs.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
