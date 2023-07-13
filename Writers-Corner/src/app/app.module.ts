e565ecb21b217f238ba5c4f4290253d88f2393fe
akshatj125 <akshatj@argusoft.com>
2023-07-13 12:52:46 +0530

    signupCreated

--- Writers-Corner/src/app/app.module.ts
+++ Writers-Corner/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { routingComponents } from './app-routing.module';
 import { HeaderComponent }  from './header/header.component'
 import { BlogsComponent } from './blogs/blogs.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BlogsComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
