361b905dc3f865db47152b7cdc5255afea2a5c25
akshatj125 <akshatj@argusoft.com>
2023-07-14 12:26:00 +0530

    checked invalid conditions for login&signup fields

--- Writers-Corner/src/app/login/login.component.ts
+++ Writers-Corner/src/app/login/login.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    userdata = new User("","");
+
+    login(){
+      console.log(this.userdata)
+    }
+}
+
+class User{
+  constructor(public username:string,public password:string) {
+    
+  }
+
 
 }
+
