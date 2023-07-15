fa26bdf60ca8cc3a25cfa64e148ae88f86360687
akshatj125 <akshatj@argusoft.com>
2023-07-15 10:06:19 +0530

    morningCommit

--- Writers-Corner/src/app/signup/signup.component.ts
+++ Writers-Corner/src/app/signup/signup.component.ts
@@ -26,9 +26,10 @@ export class SignupComponent {
   signup(){
     // console.log(this.userdata)
     const data = JSON.stringify(this.userdata)
-    this.http.post(this.url, data, this.headers).subscribe(data=>{
-      console.log(data)
-    })
+    this.http.post(this.url, data, this.headers)
+    // .subscribe(data=>{
+    //   console.log(data)
+    //})n
   }
 
 }
