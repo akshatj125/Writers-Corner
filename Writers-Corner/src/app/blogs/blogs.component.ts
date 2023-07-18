5fa4b3d41e923d7e8e585098d48ffa3e21d33106
Sufyan-007 <sgazdhar@argusoft.com>
2023-07-18 10:21:14 +0530

    css bug fixes// Subah hota he asli kaam

--- Writers-Corner/src/app/blogs/blogs.component.ts
+++ Writers-Corner/src/app/blogs/blogs.component.ts
@@ -17,14 +17,28 @@ export class BlogsComponent {
     {"id":2,"title":"Title2" , "desc":"Description.."},
     {"id":3,"title":"Title3" , "desc":"Description.."},
     {"id":4,"title":"Title4" , "desc":"Description.."},
+    {"id":1,"title":"Title1" , "desc":"Description.."},
+    {"id":2,"title":"Title2" , "desc":"Description.."},
+    {"id":3,"title":"Title3" , "desc":"Description.."},
+    {"id":4,"title":"Title4" , "desc":"Description.."},
+    {"id":1,"title":"Title1" , "desc":"Description.."},
+    {"id":2,"title":"Title2" , "desc":"Description.."},
+    {"id":3,"title":"Title3" , "desc":"Description.."},
+    {"id":4,"title":"Title4" , "desc":"Description.."},
 
   ]
-  x=4
+  x=6
   blogs=this.blogs1.slice(0,this.x)
-
+  isMoreAvailable=true
 
   IncX(){
-    this.x+=9;
+    this.x+=3;
+    console.log(this.blogs1.length)
+    if(this.blogs1.length<=this.x)
+    {
+      this.isMoreAvailable=false
+    }
+
     this.blogs=this.blogs1.slice(0,this.x)
 
   }
