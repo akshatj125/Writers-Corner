d46194f480581841cfbd92cd650406815650c91d
akshatj125 <akshatj@argusoft.com>
2023-07-18 17:38:53 +0530

    header signed up

--- Writers-Corner/src/app/header/header.component.ts
+++ Writers-Corner/src/app/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component , Input} from '@angular/core';
+import { Component , Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   @Input() 
   public EnableLogin =false;
+
+  @Input() 
+  public username ="";
+  public LoggedIn = false;
+
+  ngOnInit(): void {
+      if(this.username!==""){
+        this.LoggedIn = true;
+      }
+  }
+
 }
