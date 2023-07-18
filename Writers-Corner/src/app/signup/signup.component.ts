95983438057ec1a00cbb69f0b1355d28a018b012
Sufyan-007 <sgazdhar@argusoft.com>
2023-07-18 15:42:39 +0530

    Username verification and redirect on signup

--- Writers-Corner/src/app/signup/signup.component.ts
+++ Writers-Corner/src/app/signup/signup.component.ts
@@ -1,7 +1,8 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -12,11 +13,13 @@ export class SignupComponent {
 
   usernameExists= false
 
+  @ViewChild('username') usernameField:NgModel |undefined ;
+
   data:any;
 
   private url = "http://localhost:8080/user";
 
-  constructor(private http: HttpClient){ }
+  constructor(private http: HttpClient, private router:Router){ }
  
 
   headers={
@@ -24,14 +27,23 @@ export class SignupComponent {
       'Content-Type':'application/json'
     })
   }
-
+  signedUp :any= false;
   signup(){
     // console.log(this.userdata)
     const data = JSON.stringify(this.userdata)
-    const usernameTaken = false;
+     
     this.http.post(this.url, data, this.headers).subscribe(data=>{
       console.log(data)
+      this.signedUp=data
     })
+    if (this.signedUp)
+    {
+        this.router.navigate(['/home'])
+    }
+    else{
+      this.usernameExists=true
+      this.usernameField?.control.markAsPristine()
+    }
   }
 
 }
