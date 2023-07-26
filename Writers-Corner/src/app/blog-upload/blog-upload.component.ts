fffa5c3f62ec3536602b1e75d61c4ba0c4e62830
akshatj125 <akshatj@argusoft.com>
2023-07-26 09:49:07 +0530

    blog upload backend connected

--- Writers-Corner/src/app/blog-upload/blog-upload.component.ts
+++ Writers-Corner/src/app/blog-upload/blog-upload.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-upload',
@@ -6,12 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-upload.component.css']
 })
 export class BlogUploadComponent {
+  userId:0
   title: string 
   content: string
 
+  private url = "http://localhost:8080/createblog";
+
+  constructor(private http: HttpClient, private router:Router){ }
+ 
+  
+  headers={
+    headers:new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
   onSubmit() {
+    var data={
+      userId:this.userId,
+      "title":this.title,
+      "content":this.content
+    }
+
+    this.http.post(this.url, data, this.headers).subscribe(data=>{})
     // Now you can process the data, for example, send it to a server or store it locally
+    console.log('userId:', this.userId);
     console.log('Title:', this.title);
     console.log('Content:', this.content);
+
+    this.router.navigate(["/home"])
 }
 }
--- Writers-Corner/src/app/blogs/blogs.component.ts
+++ Writers-Corner/src/app/blogs/blogs.component.ts
@@ -1,35 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css']
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit{
 
 
-  public blogs1=[
-    {"id":1,"title":"Title1" , "desc":"Description.."},
-    {"id":2,"title":"Title2" , "desc":"Description.."},
-    {"id":3,"title":"Title3" , "desc":"Description.."},
-    {"id":4,"title":"Title4" , "desc":"Description.."},
-    {"id":1,"title":"Title1" , "desc":"Description.."},
-    {"id":2,"title":"Title2" , "desc":"Description.."},
-    {"id":3,"title":"Title3" , "desc":"Description.."},
-    {"id":4,"title":"Title4" , "desc":"Description.."},
-    {"id":1,"title":"Title1" , "desc":"Description.."},
-    {"id":2,"title":"Title2" , "desc":"Description.."},
-    {"id":3,"title":"Title3" , "desc":"Description.."},
-    {"id":4,"title":"Title4" , "desc":"Description.."},
-    {"id":1,"title":"Title1" , "desc":"Description.."},
-    {"id":2,"title":"Title2" , "desc":"Description.."},
-    {"id":3,"title":"Title3" , "desc":"Description.."},
-    {"id":4,"title":"Title4" , "desc":"Description.."},
+  public blogs1:any
+  private url = "http://localhost:8080/blogs";
 
-  ]
-  x=6
-  blogs=this.blogs1.slice(0,this.x)
+  
+ 
+  
+  headers={
+    headers:new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+  
+
+  x=3
+  blogs:any
   isMoreAvailable=true
+
+  constructor(private http: HttpClient, private router:Router){ }
+
+
+  ngOnInit(): void {
+     this.http.get(this.url,this.headers).subscribe(data=>{
+      this.blogs1=data
+      console.log(data)
+      this.IncX()
+     })
+
+
+  }
 
   IncX(){
     this.x+=3;
